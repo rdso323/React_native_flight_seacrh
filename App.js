05791f3ce10e08282809a49f6c886862b41c8955
Rohan D'Souza <rohan.dsouza01@ad.infosys.com>
2020-05-21 22:17:32 +1200

    Search ui working

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Search from './components/Search';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      origin: "N/A",
+      destination: "N/A",
+      dept_date: "2020-06-01",
+      return_date: null,
+      passengers: 0,
+      submitted: false,
+      return: false,
+    };
+  }
+
+  handleChange = (data,value) => {
+    this.setState({
+      [data]: value,
+    });
+    console.log(this.state.origin);
+  };
+
+  handleSwitch = () =>
+    this.setState({
+      return: !this.state.return,
+    })
+
+  handleSubmit = () => {
+    this.setState({
+      submitted: true
+    })
+    console.log(this.state.submitted)
+  }
+
+
+  render(){
+    return (
+      <View>
+        <Search handleChange = {this.handleChange}
+        handleSubmit = {this.handleSubmit}
+        handleSwitch = {this.handleSwitch}
+        return = {this.state.return}/>
+      </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ components/Search.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import {
+  Switch,
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+} from "react-native";
+import DatePicker from "react-native-datepicker";
+
+export class Search extends Component {
+  returnCheck() {
+    return this.props.return ? (
+      <TextInput
+        style={styles.box}
+        placeholder="Ret YYYY-MM-DD"
+        onChangeText={(value) => this.props.handleChange("return_date", value)}
+      />
+    ) : null;
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={styles.title}>
+          <Text style={{ color: "white" }}>
+            <h1>Flight Search Engine</h1>
+          </Text>
+        </View>
+        <View style={styles.searches}>
+          <View>
+            <Text>One-Way/Return</Text>
+            <Switch
+              value={this.props.return}
+              onValueChange={() => this.props.handleSwitch()}
+            />
+            <TextInput
+              style={styles.box}
+              placeholder="Enter Orgin"
+              onChangeText={(value) => this.props.handleChange("origin", value)}
+            />
+            <TextInput
+              style={styles.box}
+              placeholder="Enter Destination"
+              onChangeText={(value) =>
+                this.props.handleChange("destination", value)
+              }
+            />
+            {/* <DatePicker
+              style={styles.box}
+              placeholder="Departure Date"
+              format="YYYY-MM-DD"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              onDateChange={(value) =>
+                this.props.handleChange("dept_date", value)
+              }
+            /> */}
+            <TextInput
+              style={styles.box}
+              placeholder="Dept YYYY-MM-DD"
+              onChangeText={(value) =>
+                this.props.handleChange("dept_date", value)
+              }
+            />
+            {this.returnCheck()}
+            <TextInput
+              style={styles.box}
+              keyboardType="numeric"
+              placeholder="No of passengers"
+              onChangeText={(value) =>
+                this.props.handleChange("passengers", value)
+              }
+            />
+          </View>
+          <View style={{ marginTop: "5%" }}>
+            <Button
+              title="Submit"
+              color="red"
+              marginTop="10px"
+              onPress={() => this.props.handleSubmit()}
+            />
+          </View>
+        </View>
+        <View></View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    flex: 1,
+    backgroundColor: "#333",
+    padding: "5%",
+    alignItems: "center",
+    // justifyContent: 'center',
+  },
+  searches: {
+    borderRadius: 10,
+    marginTop: "10px",
+    width: "40%",
+    padding: "2%",
+    backgroundColor: "#39CCCC",
+  },
+  box: {
+    borderRadius: 5,
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    width: 200,
+  },
+});
+
+export default Search;
