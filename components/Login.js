15d71a6dfe4b43369f954b3a14d85f989e5e128f
Rohan D'Souza <rohan.dsouza01@ad.infosys.com>
2020-05-24 01:42:40 +1200

    Firebase working

--- components/Login.js
+++ components/Login.js
@@ -1,37 +1,55 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-//import firebase from "firebase";
+import firebase from "firebase";
 
 export default class Login extends React.Component {
-//   componentDidMount() {
-//     // Initialize Firebase
-//     var config = {
-//       apiKey: "AIzaSyBgyI5EZVOazH79ljj80WlcLJYPZZ3hYxM",
-//       authDomain: "testapp-38277.firebaseapp.com",
-//       databaseURL: "https://testapp-38277.firebaseio.com",
-//       projectId: "testapp-38277",
-//       storageBucket: "testapp-38277.appspot.com",
-//       messagingSenderId: "662181340773",
-//     };
-//     if (!firebase.apps.length) {
-//       firebase.initializeApp(config);
-//     }
-//     console.log(firebase);
-//   }
-//   createUser = () => {
-//     console.log("Button pressed");
-//     firebase
-//       .auth()
-//       .createUserWithEmailAndPassword(this.state.email, this.state.password)
-//       .then(function (response) {
-//         console.log(response.user.email + " created successfully");
-//       })
-//       .catch(function (error) {
-//         console.log(error.code);
-//         console.log(error.message);
-//       });
-//   };
+  componentDidMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBZj3kruJjiBup7OZoIc1kVKElPOgtXJ9s",
+      authDomain: "flight-search-engine-users.firebaseapp.com",
+      databaseURL: "https://flight-search-engine-users.firebaseio.com",
+      projectId: "flight-search-engine-users",
+      storageBucket: "flight-search-engine-users.appspot.com",
+      messagingSenderId: "557732276924",
+      appId: "1:557732276924:web:006639cfdb5d7e6ccd7eb9",
+      measurementId: "G-836JWQ7KW6",
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+    console.log(firebase);
+  }
+  createUser = () => {
+    console.log("Create user pressed");
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(function (response) {
+        console.log(response.user.email + " created successfully");
+      })
+      .catch(function (error) {
+        console.log(error.code);
+        console.log(error.message);
+      });
+  };
+
+  authenUser = () => {
+      console.log("Login pressed")
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        this.props.navigation.navigate("Home")
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        console.log(error.code);
+        console.log(error.message);
+        // ...
+      });
+  };
 
   render() {
     return (
@@ -39,35 +57,35 @@ export default class Login extends React.Component {
         <Text style={styles.paragraph}>Login</Text>
         <TextInput
           style={styles.textBox}
-          placeholder="Username"
-          autoCompleteType="username"
-        //   onChangeText={value => {
-        //     this.setState({
-        //       email: value,
-        //     });
-        // }}
+          placeholder="Email"
+          autoCompleteType="email"
+            onChangeText={value => {
+              this.setState({
+                email: value,
+              });
+          }}
         />
         <TextInput
           style={styles.textBox}
           placeholder="Password"
           secureTextEntry
-        //   onChangeText={value => {
-        //     this.setState({
-        //       password: value,
-        //     });
-        // }}
+            onChangeText={value => {
+              this.setState({
+                password: value,
+              });
+          }}
         />
         <View style={styles.buttons}>
           <Button
             style={{ margin: 10 }}
             title="LOGIN"
-            onPress={() => this.props.navigation.navigate("Home")}
+            onPress={() => this.authenUser()}
           />
           <Button
             style={{ margin: 10 }}
             title="Create User"
             color="red"
-            onPress={() => this.props.navigation.navigate("Home")}
+            onPress={() => this.createUser()}
           />
         </View>
       </View>
